1c780c6dd41f7e75432a954a06ca4a51de510d7f
bator <batr.fly@mail.ru>
2023-08-28 09:10:46 +0200

    fix app test

--- src/components/app/app.test.tsx
+++ src/components/app/app.test.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./app";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './app';
 
-test("renders learn react link", () => {
+test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/МБОУ АЛГОСОШ/i);
   expect(linkElement).toBeInTheDocument();
 });
