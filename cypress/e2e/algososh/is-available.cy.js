14a988039907583ddd3f35aae19e8da8f7bc30f8
bator <batr.fly@mail.ru>
2023-09-20 08:03:45 +0200

    test visit all pages

--- cypress/e2e/algososh/is-available.cy.js
+++ cypress/e2e/algososh/is-available.cy.js
@@ -2,4 +2,28 @@ describe('service is available', () => {
   it('should be available on localhost:3000', function () {
     cy.visit('http://localhost:3000');
   });
+
+  it('should by available recursion page', function () {
+    cy.visit('recursion');
+  });
+
+  it('should by available fibonacci page', function () {
+    cy.visit('fibonacci');
+  });
+
+  it('should by available sorting page', function () {
+    cy.visit('sorting');
+  });
+
+  it('should by available stack page', function () {
+    cy.visit('stack');
+  });
+
+  it('should by available queue page', function () {
+    cy.visit('queue');
+  });
+
+  it('should by available list page', function () {
+    cy.visit('list');
+  });
 });
